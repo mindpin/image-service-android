a611663900f467119f1619b33aef9e871c5dccdd
destinyd <destinyd_war@163.com>
2014-10-12 00:37:01 +0800

    image and loading

--- app/src/com/mindpin/image_service_android/UploadActivity.java
+++ app/src/com/mindpin/image_service_android/UploadActivity.java
@@ -2,6 +2,10 @@ package com.mindpin.image_service_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by dd on 14-10-11.
@@ -11,5 +15,16 @@ public class UploadActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload);
+        init_views();
+    }
+
+    private void init_views() {
+        Animation operatingAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        LinearInterpolator lin = new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+        if (operatingAnim != null) {
+            findViewById(R.id.fatv_loading).startAnimation(operatingAnim);
+        }
+
     }
 }
